c27a5b24c6e867039fab9efc4fc42166e91a916c
Aman Bahata <bahataaman@gmail.com>
2020-03-08 13:11:06 +0000

    Adds CORS configuration

--- src/main/java/com/aman/inventory_management/InventoryManagementApplication.java
+++ src/main/java/com/aman/inventory_management/InventoryManagementApplication.java
@@ -2,6 +2,9 @@ package com.aman.inventory_management;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @SpringBootApplication
 public class InventoryManagementApplication {
@@ -10,4 +13,13 @@ public class InventoryManagementApplication {
 		SpringApplication.run(InventoryManagementApplication.class, args);
 	}
 
+	@Bean
+	public WebMvcConfigurer corsConfigurer(){
+		return new WebMvcConfigurer() {
+			@Override
+			public void addCorsMappings(CorsRegistry registry) {
+				registry.addMapping("javaconfig").allowedOrigins("https://inventory-management-ui.herokuapp.com");
+			}
+		};
+	}
 }
